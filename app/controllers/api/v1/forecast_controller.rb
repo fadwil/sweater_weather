00a9be9aff67b139849cfc50f7804e71638fd6cb
Wil Fady <wrfady@gmail.com>
2023-09-27 11:41:07 -0400

    error handling forecast

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    coordinates = MapquestFacade.fetch_lat_lng(params[:location])
-    forecast = ForecastFacade.fetch_forecast(coordinates)
-    render json: ForecastSerializer.new(forecast)
+    if params[:location].nil?
+      render json: {error: "Please provide a location."}, status: 400
+    else
+      coordinates = MapquestFacade.fetch_lat_lng(params[:location])
+      forecast = ForecastFacade.fetch_forecast(coordinates)
+      render json: ForecastSerializer.new(forecast)
+    end
   end
 end
